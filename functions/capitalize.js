c5de9056e2ec40e3d1c7afd56a537b3cbf92f734
matt <mattcave814@gmail.com>
2022-10-28 21:01:52 -0500

    capitalize function and all tests complete

--- functions/capitalize.js
+++ functions/capitalize.js
@@ -1,4 +1,5 @@
 export default function capitalize(str) {
+    if (str == '' || typeof str != 'string') return '';
     let split = str.toLowerCase().split('');
     split[0] = split[0].toUpperCase();
     return split.join('');
--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,5 +1,25 @@
 import capitalize from '../functions/capitalize';
 
-test('first test', () => {
-    expect(capitalize('mattheW')).toBe('Matthew');
+test('all lower case', () => {
+    expect(capitalize('matthew')).toBe('Matthew');
+});
+
+test('all upper case', () => {
+    expect(capitalize('MATTHEW')).toBe('Matthew');
+});
+
+test('mixed case', () => {
+    expect(capitalize('MaTtHeW')).toBe('Matthew');
+});
+
+test('blank string input', () => {
+    expect(capitalize('')).toBe('');
+});
+
+test('non-string input', () => {
+    expect(capitalize(52)).toBe('');
+});
+
+test('multiple words input', () => {
+    expect(capitalize('the pUrplE monKey')).toBe('The purple monkey');
 });
